5a32e1ce9bbb993015ed9e1082d909cefe40efe1
jessica-nagar <77754279+jessica-nagar@users.noreply.github.com>
2021-02-08 20:04:41 -0500

    AM/PM update

--- ClockDisplay.java
+++ ClockDisplay.java
@@ -16,6 +16,7 @@ public class ClockDisplay
 {
     private NumberDisplay hours;
     private NumberDisplay minutes;
+    private String amPm;
     private String displayString;    // simulates the actual display
     
     /**
@@ -34,10 +35,11 @@ public class ClockDisplay
      * creates a new clock set at the time specified by the 
      * parameters.
      */
-    public ClockDisplay(int hour, int minute)
+    public ClockDisplay(int hour, int minute, String am_pm)
     {
         hours = new NumberDisplay(13);
         minutes = new NumberDisplay(60);
+        amPm = am_pm;
         setTime(hour, minute);
     }
 
@@ -53,6 +55,10 @@ public class ClockDisplay
             {
                 hours.increment();
                 hours.increment();
+                if(amPm == "am")
+                    amPm = "pm";
+                else
+                    amPm = "am";
             }
             else
             {
@@ -87,6 +93,6 @@ public class ClockDisplay
     private void updateDisplay()
     {
         displayString = hours.getDisplayValue() + ":" + 
-                        minutes.getDisplayValue();
+                        minutes.getDisplayValue() + " " + amPm;
     }
 }
